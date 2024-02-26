819754680efb0c1cc39bcb014a5df6be33fa5e64
Yusuf Van Gieson <yusufvg98@gmail.com>
2024-02-25 16:22:19 -0800

    fix styling

---
+++ src/components/TestPanel.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// import "@/assets/index.css";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+</script>
+
+<template>
+  <h1 class="text-gray-950 underline">Test</h1>
+
+  <Card class="m-2">
+    <CardHeader>
+      <CardTitle>Card Title</CardTitle>
+      <CardDescription>Card Description</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <p>Card Content</p>
+    </CardContent>
+    <CardFooter>
+      <p>Card Footer</p>
+    </CardFooter>
+  </Card>
+</template>
